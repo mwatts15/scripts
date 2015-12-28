45e58415a09c341f270a0a8ddd28890c62c1886d
Mark Watts <wattsmark2015@gmail.com>
2015-12-28 10:28:34 -0600

    j2.sh: Made the `cowsay` title format optional
    - Also, replaced a statically named temp file with a mktemp call

--- j2.sh
+++ j2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-JOURNAL_DIR=$HOME/journal
+JOURNAL_DIR=${JOURNAL_DIR:-$HOME/journal}
 GPG_NAME=${GPG_NAME:-$USER}
+DO_SILLY_TITLE=${DO_SILLY_TITLE:-1}
+DO_ENCRYPT=${DO_ENCRYPT:-1}
 
 while [ $# -gt 0 ]; do 
     case $1 in
@@ -21,6 +23,7 @@ while [ $# -gt 0 ]; do
             ;;
     esac
 done
+
 if [ $doread ] ; then
     cat $readlist | less
     exit
@@ -38,62 +41,89 @@ center()
      fi
 }
 
+plain_title()
+{
+    tit="${1}"
+    out="${2}"
+    bar=`echo "$tit" | sed s/./-/g`
+    center "  +$bar+  " >> "${out}"
+    center "-= $tit =-" | tr '[a-z]' '[A-Z]' | cat >> "${out}"
+    center "  +$bar+  " >> "${out}"
+}
+
+if [ ! -d $JOURNAL_DIR ] ; then
+    echo Making journal directory: $JOURNAL_DIR
+    mkdir $JOURNAL_DIR
+fi
 cd $JOURNAL_DIR
 
 fname=`date +%Y%m%d%H%M%S`
-timestamp=`date --rfc-3339=seconds`
+timestamp=`date --rfc-3339=seconds 2>/dev/null`
+if [ ! $? -eq 1 ] ; then
+    timestamp=`date +%Y-%m-%d %H:%M:%S%z 2>/dev/null`
+fi
 
-#title="$@"
+dt=`mktemp`
+
 # `dt' is just where we put the 
 # Date and Title
 title="${title:============}"
-case $(( $RANDOM % 19)) in
+
+case $(( ($RANDOM % 19) * $DO_SILLY_TITLE)) in
     0)
-        cowsay -f "eyes" $title >>dt
+        plain_title "$title" ${dt}
         ;;
     1)
-        cowsay -f "tux" $title >>dt
+        cowsay -f "eyes" "$title" >>${dt}
         ;;
     2)
-        cowsay -f "kitty" $title >>dt
+        cowsay -f "tux" "$title" >>${dt}
         ;;
     3)
-        cowsay -f "bong" $title >>dt
+        cowsay -f "kitty" "$title" >>${dt}
+        ;;
+    4)
+        cowsay -f "bong" "$title" >>${dt}
         ;;
     *)
-        bar=`echo "$title" | sed s/./-/g`
-        center "  +$bar+  " >> "dt"
-        center "-= $title =-" | tr '[a-z]' '[A-Z]' | cat >> "dt"
-        center "  +$bar+  " >> "dt"
+        plain_title "$title" ${dt}
         ;;
 esac
-echo >>dt
-echo >>dt
-echo --$USER >>dt
-echo $timestamp >>dt
-
-echo $EDITOR
+echo >>${dt}
+echo >>${dt}
+echo --$USER >>${dt}
+echo $timestamp >>${dt}
+vim_newentry_map="nmap ,l o<ESC>!!date +\\\\%_H\\\\%M<CR>i<End>"
 case $EDITOR in
     *vim|*vi)
-        vim +"read dt" +"set wrap" +"set spell" +"startinsert" +"set tw=0" +/^$/ $fname
+        vim +"read ${dt}" +"set wrap" +"set spell" +"exec \"$vim_newentry_map\"" +"startinsert" +"set tw=0" +/^$/ $fname
         ;;
     *nano)
-        cat dt | $EDITOR --softwrap $fname
+        cat ${dt} | $EDITOR --softwrap $fname
         ;;
     *)
         $EDITOR $fname
         ;;
 esac
 
+exit
+
 # remove temporary file
-rm dt
+rm ${dt}
 
 if [ -e $fname ] ; then
+    tmp0=`mktemp`
+    tmp1=`mktemp`
     # the gpg line assumes your username is in your keyring
-    fold -s $fname \
-    | gpg -e -r $GPG_NAME  \
-    >tmp
-    mv tmp $fname
+    fold -s $fname > $tmp0
+    if [ $DO_ENCRYPT -eq 1 ] ; then
+        echo Encrypting entry for $GPG_NAME
+        gpg -e -r $GPG_NAME < $tmp0 > $tmp1
+    else
+        cat < $tmp0 > $tmp1
+    fi
+    mv ${tmp1} $fname
+    rm ${tmp0}
 else
     echo Did not create journal entry
 fi
