e1da151b02dde2357e0599278444ac6e0e027b5c
Mark Watts <wattsmark2015@gmail.com>
2017-01-08 19:24:48 -0600

    Made the toggle work for my touchpad

--- mouse_toggle.sh
+++ mouse_toggle.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-id=$(xinput list | egrep ".*core.*pointer.*" | egrep -o -i -m 1 "id=[[:digit:]]+" | egrep -o "[[:digit:]]+")
+id=$(xinput list | egrep "TouchPad" | egrep -o -i -m 1 "id=[[:digit:]]+" | egrep -o "[[:digit:]]+")
 kv_enabled=( $(xinput list-props $id | egrep "Device Enabled" | egrep -o "[[:digit:]]+") )
+kv_enabled_prop=${kv_enabled[0]}
+kv_enabled=${kv_enabled[1]}
 
-echo $id ${kv_enabled[2]}
-
-if [ x"${kv_enabled[2]}" = x1 ] ; then
+if [ x"${kv_enabled}" = x1 ] ; then
     new_status=0
 else
     new_status=1
 fi
 
-echo xinput set-prop $id ${kv_enabled[1]} $new_status
-xinput set-int-prop $id ${kv_enabled[1]} $new_status
+xinput set-prop $id ${kv_enabled_prop} $new_status
