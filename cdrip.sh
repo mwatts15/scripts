320399e5429ec0b2f288c9eb2248b17f3f856b3c
Mark Watts <watts.mark2015@gmail.com>
2017-11-24 12:05:21 -0600

    cdrip.sh: Modifed to rip CD in a remote machine

--- cdrip.sh
+++ cdrip.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 DEVICE=${1:-/dev/cdrom}
-cdparanoia -ve -w -B -d $DEVICE 
-flac *.wav
-rm *.wav
-picard *.flac
+RHOST=${CDRIP_RHOST:?Must have a CDRIP_RHOST value set to specify the machine where the cdrip actually happens}
+dir=`date +%Y%m%d%S`
+mkdir -p $HOME/music/$dir
+ssh $RHOST "mkdir -p $dir"
+ssh $RHOST "cd $dir && cdparanoia -ve -w -B -d $DEVICE"
+ssh $RHOST "cd $dir && flac *.wav"
+ssh $RHOST "cd $dir && rm *.wav"
+scp -r "$RHOST:$dir/*" $HOME/music/$dir
+cd $HOME/music/$dir && find -name '*.flac' | xargs picard
